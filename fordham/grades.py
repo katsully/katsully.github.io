345bbf5100b5f8671d1ac5f8298879d384caee5d
Kat Sullivan <kmsullivan012@gmail.com>
2018-10-18 13:24:18 -0400

    update to syllabus

--- fordham/grades.py
+++ fordham/grades.py
@@ -1,6 +1,5 @@
 import csv
 
-sandra=False
 with open('grading/Sheet1.csv', 'r') as infile:
 	reader = csv.reader(infile)
 	first_line = True
@@ -20,15 +19,9 @@ with open('grading/Sheet1.csv', 'r') as infile:
 		midterm = 0
 		final = 0
 		if row[0].rfind(' ') != -1:
-			n = row[0][:row[0].rfind(' ')].replace(' ', '')
-			print(n)
-			if n == 'SandraCruz':
-				print("HERE")
-				sandra=True
-			else:
-				sandra=False
-		else: 
-			n = row[0].split(' ')[0]
+			n = "".join(row[0].split(" ")[:2][::-1]).replace(',','')
+		# else: 
+		# 	n = row[0].split(' ')[0]
 		name = ''.join(n.split(',')[::-1])
 		student_file = open("grading/" + name + ".txt", 'w')
 		student_file.write("Name: " + ' '.join(n.split(',')[::-1]) + "\n")
@@ -40,7 +33,6 @@ with open('grading/Sheet1.csv', 'r') as infile:
 						continue
 					homeworks.append(int(r))
 					hw_list += headers[idx] + ": " + r + "\n"
-					print(homeworks)
 				if "Quiz" in headers[idx]:
 					if r == '-':
 						continue
@@ -58,7 +50,7 @@ with open('grading/Sheet1.csv', 'r') as infile:
 					final = float(r)
 		
 		#drop lowest homework
-		homeworks.remove(min(homeworks))
+	#	homeworks.remove(min(homeworks))
 
 		student_file.write("\n" + q_list)
 		if q_count > 0:
@@ -68,13 +60,14 @@ with open('grading/Sheet1.csv', 'r') as infile:
 		hw_grade = round(sum(homeworks)/len(homeworks))
 		student_file.write("Quiz Grade: " + str(quiz_grade) + "\n")
 		student_file.write("\n" + hw_list)
-		student_file.write("Homework Grade (with lowest grade dropped): " + str(hw_grade) + "\n")
+		# student_file.write("Homework Grade (with lowest grade dropped): " + str(hw_grade) + "\n")
+		student_file.write("Homework Grade: " + str(hw_grade) + "\n")
 		student_file.write("\nClass Participation: 100" + "\n")
 		# student_file.write("\nClass Participation: " + str(class_participation*10) + "\n")
 
-		student_file.write("\nMidterm Grade: " + str(midterm) + "\n")
+		# student_file.write("\nMidterm Grade: " + str(midterm) + "\n")
 
-		student_file.write("\nFinal Grade: " + str(final) + "\n")
+		# student_file.write("\nFinal Grade: " + str(final) + "\n")
 
 		student_file.write("\nStudents are allowed one unexcused absence, after that each absence results in a grade deduction.")
 		student_file.write("\nThree tardies will result in an unexcused absence.")
@@ -94,22 +87,22 @@ with open('grading/Sheet1.csv', 'r') as infile:
 		student_file.write("\nGrade deductions: " + str(deduction))
 
 
-		grade = round((quiz_grade * .15) + (hw_grade * .4) + class_participation + (midterm * .15) + (final * .2) - (deduction))
-		# grade = math.ceil((quiz_grade * .15) + (hw_grade * .4) + class_participation - (1.5*deduction))
-		# grade1 = math.ceil(grade + 35)
-		# grade2 = math.ceil(grade + (35 * .8))
-		# grade1 = math.ceil(grade + 20)	
+		#grade = math.ceil((quiz_grade * .15) + (hw_grade * .4) + class_participation + (midterm * .15) + (final * .2) - (deduction))
+		grade = round((quiz_grade * .15) + (hw_grade * .4) + class_participation - (1.5*deduction))
+		grade1 = round(grade + 35)
+		grade2 = round(grade + (35 * .8))
+		# grade1 = math.ceil(grade + 20)
 		# grade2 = math.ceil(grade + (20 * .8))
 
-		# student_file.write("\n\nPotential grade if you get a 100 on the midterm and final: " + str(grade1))
-		# student_file.write("\nPotential grade if you get a 80 on the midterm and final: " + str(grade2))
+		student_file.write("\n\nPotential grade if you get 100% on the midterm and final: " + str(grade1))
+		student_file.write("\nPotential grade if you get 80% on the midterm and final: " + str(grade2))
 
 		# student_file.write("\n\nPotential grade if you get a 100 on the final: " + str(grade1))
 		# student_file.write("\nPotential grade if you get a 80 on the final: " + str(grade2))
 
-		student_file.write("\nYOUR FINAL GRADE: " + str(grade))
+		# student_file.write("\nYOUR FINAL GRADE: " + str(grade))
 		
-		# student_file.write("\n\nPlease note, class participation will not be an automatic 100 moving forward in the semester. Also, there will be more quizzes and homeworks that can also improve your final grade. Feel free to email me with any comments, questions, or concerns.")
+		student_file.write("\n\nPlease note, class participation will not be an automatic 100 moving forward in the semester. Also, there will be more quizzes and homeworks that can also improve your final grade. Feel free to email me with any comments, questions, or concerns.")
 		student_file.close()
 
 infile.close()
